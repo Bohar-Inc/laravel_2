e637d5aa9cd8e748aec590d94e1c00dc446592c9
PURUS01 <purus5718@gmail.com>
2024-12-27 15:20:57 +0530

    Add some view to dashboard page and order navigation buttons

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -25,9 +25,11 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
-
     protected static ?string $navigationIcon = 'heroicon-o-user-group';
 
+    protected static ?string $recordTitleAttribute = 'name';
+
+    protected static ?int $navigationSort = 1;
     public static function form(Form $form): Form
     {
         return $form
@@ -93,6 +95,11 @@ class UserResource extends Resource
         return [
             RelationManagers\OrdersRelationManager::class,
         ];
+    }
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['name', 'email'];
     }
 
     public static function getPages(): array
