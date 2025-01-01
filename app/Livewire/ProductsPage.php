3ed93d09c7eb10295d02878a6b7ca484b5530b83
PURUS01 <purus5718@gmail.com>
2025-01-01 15:53:50 +0530

    add click fuction using livewire and install livewire sweet lalert to show alert message after add to cart

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,9 +2,12 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\Title;
 use Livewire\Attributes\Url;
 use Livewire\Component;
@@ -13,6 +16,8 @@ use Livewire\WithPagination;
 #[Title('Products Page - Laravel Filament')]
 class ProductsPage extends Component
 {
+    use LivewireAlert;
+    use WithPagination;
     #[Url]
     public $selected_categories=[];
 
@@ -29,9 +34,21 @@ class ProductsPage extends Component
     public $price_range=300000;
 
     #[Url]
-    public $sort;
+    public $sort='latest';
 
-    use WithPagination;
+    //add product to cart method
+    public function addToCart($product_id)
+    {
+        $total_count=CartManagement::addItemToCart($product_id);
+        $this->dispatch('update-cart-count',total_count:$total_count)->to(Navbar::class);
+
+        $this->alert('success','Product added to cart successfully!',[
+            'position' =>  'top-end',
+            'timer' =>  3000,
+            'toast' =>  true,
+        ]);
+    }
+
     public function render()
     {
         $productQuery=Product::query()->where('is_active',1);
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -14,6 +14,8 @@
             {{ $slot }}
         </main>
     @livewire('partials.footer')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <x-livewire-alert::scripts />
     @livewireScripts
     </body>
 </html>
