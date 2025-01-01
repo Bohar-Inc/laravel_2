fec31fc72aed798d2107e0f7c2f8aa53e39b29fa
PURUS01 <purus5718@gmail.com>
2025-01-01 11:28:54 +0530

    List the categories ,brands and products in product page using livewire

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,14 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 #[Title('Products Page - Laravel Filament')]
 class ProductsPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.products-page');
+        $productQuery=Product::query()->where('is_active',1);
+        $brands=Brand::where('is_active',1)->get(['id','name','slug']);
+        $categories=Category::where('is_active',1)->get(['id','name','slug']);
+        return view('livewire.products-page',['products'=>$productQuery->paginate(6),'brands'=>$brands,'categories'=>$categories]);
     }
 }
