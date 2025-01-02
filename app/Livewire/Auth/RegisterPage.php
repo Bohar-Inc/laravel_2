9c433a63c895d26abe111af2353cd549f4147cfe
PURUS01 <purus5718@gmail.com>
2025-01-02 09:04:21 +0530

    register page wire model set

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class RegisterPage extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+
+    public function save()
+    {
+        dd($this->name, $this->email, $this->password);
+    }
     public function render()
     {
         return view('livewire.auth.register-page');
