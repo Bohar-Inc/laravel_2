36989337dbc644a0209661ec436bb2473a8e4a4f
SenthVyra <88665212+SenthVyra@users.noreply.github.com>
2025-01-04 11:47:40 +0530

    Video 1
    Create all the tables

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,21 +3,23 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // Add this line
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
      * Bootstrap any application services.
      */
     public function boot(): void
+    {
+        // Set default string length to 191 characters
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
     {
         //
     }
